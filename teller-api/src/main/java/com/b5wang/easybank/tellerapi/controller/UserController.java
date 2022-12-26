c4414fa0dacadb790c0e5aaad7f0f1f32657de6d
B5WANG <neo.b5wang@gmail.com>
2022-12-27 00:04:34 +0800

    teller-ui-react
    teller-ui-react

---
+++ teller-api/src/main/java/com/b5wang/easybank/tellerapi/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.b5wang.easybank.tellerapi.controller;
+
+import com.b5wang.easybank.tellerapi.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestController
+public class UserController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/users/status")
+    @ResponseBody
+    public String status(){
+        log.info("Status request");
+        return "OK";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/users/list")
+    @ResponseBody
+    public ResponseEntity<List<User>> list(){
+        User u1 = new User();
+        u1.setId("1");
+        u1.setUsername("Tom");
+        User u2 = new User();
+        u2.setId("2");
+        u2.setUsername("Peter");
+        User u3 = new User();
+        u3.setId("3");
+        u3.setUsername("ddT");
+
+        List list = new ArrayList<User>();
+        list.add(u1);
+        list.add(u2);
+        list.add(u3);
+
+        return ResponseEntity.ok(list);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/users/login")
+    @ResponseBody
+    public String login(@RequestParam String username, @RequestParam String password){
+        log.info("login, username: {}, password: {}", username, password);
+        return "OK";
+    }
+
+
+
+
+}
