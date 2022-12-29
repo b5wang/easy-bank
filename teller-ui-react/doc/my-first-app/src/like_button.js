d467d89c60d4bd9e3e36698694cda010a968518e
B5WANG <neo.b5wang@gmail.com>
2022-12-30 00:44:07 +0800

    my first app - react
    my first app - react

--- teller-ui-react/doc/my-first-app/src/like_button.js
+++ teller-ui-react/doc/my-first-app/src/like_button.js
@@ -22,8 +22,8 @@ class LikeButton extends React.Component {
 }
 
 // Find all DOM containers, and render Like buttons into them.
-document.querySelectorAll('.like_button_container')
-    .forEach(domContainer => {
+document.querySelectorAll('.like_button_container').forEach(
+    domContainer => {
         // Read the comment ID from a data-* attribute.
         const commentID = parseInt(domContainer.dataset.commentid, 10);
         const root = ReactDOM.createRoot(domContainer);
